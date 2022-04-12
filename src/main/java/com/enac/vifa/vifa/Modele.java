cad77a93a3c30413fd74b06a0090f77493a43a5e
Victor755431 <azarbidu54@gmail.com>
2022-04-12 13:38:18 +0200

    VIFA-39: Getters et setters du modèle.

--- src/main/java/com/enac/vifa/vifa/Modele.java
+++ src/main/java/com/enac/vifa/vifa/Modele.java
@@ -2,6 +2,8 @@ package com.enac.vifa.vifa;
 
 import java.util.ArrayList;
 
+import javafx.geometry.Point3D;
+
 public class Modele {
     private ArrayList<Forme2D> listeDesFormes;
     private double mass;
@@ -21,4 +23,178 @@ public class Modele {
     private double p;
     private double q;
     private double r;
+    
+    //CONSTRUCTOR
+
+    public Modele() {
+        this.listeDesFormes = new ArrayList<Forme2D>();
+        this.mass = 0;
+        this.xCentrage=0;
+        this.vAir= 0;
+        this.psi=0;
+        this.theta=0;
+        this.phi=0;
+        this.alpha=0;
+        this.beta=0;
+        this.a0=0;
+        this.trim=0;
+        this.dl=0;
+        this.dm=0;
+        this.dn=0;
+        this.dx=0;
+        this.p=0;
+        this.q=0;
+        this.r=0;
+    }
+
+ //GETTERS AND SETTERS
+
+    public double getMass() {
+        return mass;
+    }
+
+    public void setMass(double mass) {
+        this.mass = mass;
+    }
+
+    public double getxCentrage() {
+        return xCentrage;
+    }
+
+    public void setxCentrage(double xCentrage) {
+        this.xCentrage = xCentrage;
+    }
+
+    public double getvAir() {
+        return vAir;
+    }
+
+    public void setvAir(double vAir) {
+        this.vAir = vAir;
+    }
+
+    public double getPsi() {
+        return psi;
+    }
+
+    public void setPsi(double psi) {
+        this.psi = psi;
+    }
+
+    public double getTheta() {
+        return theta;
+    }
+
+    public void setTheta(double theta) {
+        this.theta = theta;
+    }
+
+    public double getPhi() {
+        return phi;
+    }
+
+    public void setPhi(double phi) {
+        this.phi = phi;
+    }
+
+    public double getAlpha() {
+        return alpha;
+    }
+
+    public void setAlpha(double alpha) {
+        this.alpha = alpha;
+    }
+
+    public double getBeta() {
+        return beta;
+    }
+
+    public void setBeta(double beta) {
+        this.beta = beta;
+    }
+
+    public double getA0() {
+        return a0;
+    }
+
+    public void setA0(double a0) {
+        this.a0 = a0;
+    }
+
+    public double getTrim() {
+        return trim;
+    }
+
+    public void setTrim(double trim) {
+        this.trim = trim;
+    }
+
+    public double getDl() {
+        return dl;
+    }
+
+    public void setDl(double dl) {
+        this.dl = dl;
+    }
+
+    public double getDm() {
+        return dm;
+    }
+
+    public void setDm(double dm) {
+        this.dm = dm;
+    }
+
+    public double getDn() {
+        return dn;
+    }
+
+    public void setDn(double dn) {
+        this.dn = dn;
+    }
+
+    public double getDx() {
+        return dx;
+    }
+
+    public void setDx(double dx) {
+        this.dx = dx;
+    }
+
+    public double getP() {
+        return p;
+    }
+
+    public void setP(double p) {
+        this.p = p;
+    }
+
+    public double getQ() {
+        return q;
+    }
+
+    public void setQ(double q) {
+        this.q = q;
+    }
+
+    public double getR() {
+        return r;
+    }
+
+    public void setR(double r) {
+        this.r = r;
+    }
+    
+    public void addForme (String nom){
+        this.listeDesFormes.add(new Forme2D(nom));
+    }
+
+    public void addPointToForme (String nom, double x, double y, double z){
+        for (Forme2D f:listeDesFormes){
+            if (f.getNom().equals(nom)){
+                f.addPoint(new Point3D(x, y, z));
+                break;
+            }
+        }
+    }
 }
