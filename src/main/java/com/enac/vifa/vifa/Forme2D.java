784e41ab7b0fe6a6febe43b8b5cb0d76a0cdb2f6
Victor755431 <azarbidu54@gmail.com>
2022-04-12 11:41:49 +0200

    VIFA-39: Début du code de l'interface Ivy, opens VIFA-39

---
+++ src/main/java/com/enac/vifa/vifa/Forme2D.java
@@ -0,0 +1,8 @@
+package com.enac.vifa.vifa;
+
+import java.util.ArrayList;
+
+public class Forme2D {
+    private String nom;
+    private ArrayList<Triplet<double,double,double>> contour;
+}
---
+++ src/main/java/com/enac/vifa/vifa/Modele.java
@@ -0,0 +1,24 @@
+package com.enac.vifa.vifa;
+
+import java.util.ArrayList;
+
+public class Modele {
+    private ArrayList<Forme2D> listeDesFormes;
+    private double mass;
+    private double xCentrage;
+    private double vAir;
+    private double psi;
+    private double theta;
+    private double phi;
+    private double alpha;
+    private double beta;
+    private double a0;
+    private double trim;
+    private double dl;
+    private double dm;
+    private double dn;
+    private double dx;
+    private double p;
+    private double q;
+    private double r;
+}
