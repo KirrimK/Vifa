b1d182c4064b8c9d45fd48c669a797822370bdbd
KirrimK <7959215-KirrimK@users.noreply.gitlab.com>
2022-04-13 23:49:48 +0200

    Added other elements to demo, scrool zoom test

--- src/main/java/com/enac/vifa/vifa/Main.java
+++ src/main/java/com/enac/vifa/vifa/Main.java
@@ -1,6 +1,7 @@
 package com.enac.vifa.vifa;
 
 import javafx.application.Application;
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleListProperty;
 import javafx.beans.value.ChangeListener;
@@ -11,44 +12,104 @@ import javafx.scene.control.Slider;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
-import javafx.scene.shape.Box;
+import javafx.scene.shape.*;
 import javafx.scene.transform.Rotate;
 import javafx.scene.transform.Translate;
 import javafx.stage.Stage;
 import org.fxyz3d.geometry.Point3D;
 import org.fxyz3d.shapes.composites.PolyLine3D;
+import org.fxyz3d.shapes.polygon.PolygonMesh;
+import org.fxyz3d.shapes.primitives.SpringMesh;
+import org.fxyz3d.shapes.primitives.Surface3DMesh;
+import org.fxyz3d.shapes.primitives.TexturedMesh;
+import org.fxyz3d.shapes.primitives.TriangulatedMesh;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
+
+import static java.lang.Double.max;
 
 //import com.enac.vifa.vifa.Vue3D;
 
 public class Main extends Application {
-    public Parent createContent() throws Exception {
+    public Parent createContent(Scene mainScene) throws Exception {
         // Box
-        Box testBox = new Box(5, 5, 5);
+        Box testBox = new Box(5, 0, 5);
         testBox.setMaterial(new PhongMaterial(Color.RED));
         //testBox.setDrawMode(DrawMode.LINE);
 
-        ArrayList<Point3D> bruh_ = new ArrayList<Point3D>();
-        bruh_.add(new Point3D(10, 10, 10));
-        bruh_.add(new Point3D(20, 20, 20));
-        bruh_.add(new Point3D(30, 30, 30));
-        PolyLine3D bruh = new PolyLine3D(bruh_, 20, Color.GREEN);
+        Cylinder bruh = new Cylinder(5, 0, 6);
+        bruh.setMaterial(new PhongMaterial(Color.BLUE));
+        bruh.setTranslateY(5);
 
+        float smallSize = 4.0f;
+        float bigSize = 6.0f;
+        float high = 3.0f;
+        float depth = 1.0f;
+
+        TriangleMesh m = new TriangleMesh();
+        float s = ((float)smallSize) ;
+        float b = ((float)bigSize);
+        float h = ((float)high);
+        float d = ((float)depth);
+
+        //create Points
+        m.getPoints().addAll(
+                -s/2 , -h/2 ,  d/2,	// A = 0
+                s/2 , -h/2 ,  d/2,	// B = 1
+                -b/2 ,  h/2 ,  d/2,	// C = 2
+                b/2 ,  h/2 ,  d/2,	// D = 3
+                -s/2 , -h/2 , -d/2,	// E = 4
+                s/2 , -h/2 , -d/2,	// F = 5
+                -b/2 ,  h/2 , -d/2,	// G = 6
+                b/2 ,  h/2 , -d/2	// H = 7
+        );
+
+        m.getTexCoords().addAll(0,0);
+
+        m.getFaces().addAll(
+                0 , 0 , 1 , 0 , 3 , 0 ,		// A-B-D
+                0 , 0 , 3 , 0 , 2 , 0 , 	// A-D-C
+                0 , 0 , 2 , 0 , 6 , 0 ,		// A-C-G
+                0 , 0 , 6 , 0 , 4 , 0 , 	// A-G-E
+                0 , 0 , 4 , 0 , 1 , 0 ,		// A-E-B
+                1 , 0 , 4 , 0 , 5 , 0 , 	// B-E-F
+                1 , 0 , 5 , 0 , 7 , 0 ,		// B-F-H
+                1 , 0 , 7 , 0 , 3 , 0 ,		// B-H-D
+                3 , 0 , 7 , 0 , 6 , 0 ,		// D-H-G
+                3 , 0 , 6 , 0 , 2 , 0 ,		// D-G-C
+                6 , 0 , 7 , 0 , 5 , 0 ,		// G-H-F
+                6 , 0 , 5 , 0 , 4 , 0		// G-F-E
+        );
+
+        MeshView mm = new MeshView(m);
+
+        /*ArrayList<Point3D> bruh_ = new ArrayList<Point3D>();
+        bruh_.add(new Point3D(0, 0, 0));
+        bruh_.add(new Point3D(0, 20, 0));
+        bruh_.add(new Point3D(0, 0, 20));
+        bruh_.add(new Point3D(0, 0, 0));
+
+        TexturedMesh spring = new TriangulatedMesh(bruh_);
+        spring.setCullFace(CullFace.NONE);
+        //spring.setTextureModeVertices3D(1530, p -> p.f);
+        spring.setDrawMode(DrawMode.FILL);*/
 
         // Create and position camera
         SimpleIntegerProperty xprop = new SimpleIntegerProperty();
         xprop.set(0);
         SimpleIntegerProperty yprop = new SimpleIntegerProperty();
         yprop.set(0);
-        SimpleIntegerProperty zoomprop = new SimpleIntegerProperty();
+        SimpleDoubleProperty zoomprop = new SimpleDoubleProperty();
         zoomprop.set(15);
         PerspectiveCamera camera = new PerspectiveCamera(true);
+        camera.setFarClip(300.0f);
         camera.getTransforms().setAll (
                 new Rotate(yprop.get(), Rotate.Y_AXIS),
                 new Rotate(xprop.get(), Rotate.X_AXIS),
                 new Translate(0, 0, -zoomprop.get()));
+
         xprop.addListener(new ChangeListener<Number>() {
             @Override
             public void changed(ObservableValue<? extends Number> observableValue, Number number, Number t1) {
@@ -75,28 +136,34 @@ public class Main extends Application {
         root.getChildren().add(camera);
         root.getChildren().add(testBox);
         root.getChildren().add(bruh);
+        root.getChildren().add(mm);
+        //root.getChildren().add(spring);
 
         // Use a SubScene
-        SubScene subScene = new SubScene(root, 400, 400);
+        SubScene subScene = new SubScene(root, 400, 400, true, SceneAntialiasing.BALANCED);
+        subScene.heightProperty().bind(mainScene.heightProperty());
+        subScene.widthProperty().bind((mainScene.widthProperty()));
         subScene.setFill(Color.ALICEBLUE);
         subScene.setCamera(camera);
+
+        float MIN_VALUE = 25;
+        subScene.setOnScroll((event) -> {
+            zoomprop.set(max(event.getDeltaY()/2 + zoomprop.get(), MIN_VALUE));
+        });
+
         Group group = new Group();
         group.getChildren().add(subScene);
         // create camera control tool (test)
         HBox hbox = new HBox();
-        Slider xsl = new Slider(0, 360, 1);
+        Slider xsl = new Slider(-180, 180, 1);
         xprop.bind(xsl.valueProperty());
-        //xprop.bindBidirectional(xsl.valueProperty());
-        Slider ysl = new Slider(0, 360, 1);
+        Slider ysl = new Slider(-180, 180, 1);
         yprop.bind(ysl.valueProperty());
-        //yprop.bindBidirectional(ysl.valueProperty());
-        Slider zoomsl = new Slider(1, 100, 1);
-        zoomprop.bind(zoomsl.valueProperty());
-        zoomsl.setValue(50);
-        //zoomprop.bindBidirectional(zoomsl.valueProperty());
+        xsl.setValue(0);
+        ysl.setValue(0);
+        zoomprop.set(50);
         hbox.getChildren().add(xsl);
         hbox.getChildren().add(ysl);
-        hbox.getChildren().add(zoomsl);
         group.getChildren().add(hbox);
         return group;
     }
@@ -104,7 +171,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         //primaryStage.setResizable(false);
-        Scene scene = new Scene(createContent());
+        Scene scene = new Scene(new Group(), 800, 800);
+        scene.setRoot(createContent(scene));
         primaryStage.setScene(scene);
         primaryStage.show();
     }
