176ab03edf9abe16dee34871014a6e9e1c5d73dd
Victor755431 <11173617-vdecrevoisier@users.noreply.gitlab.com>
2022-05-13 12:42:57 +0200

    Import inutiles en moins

--- src/main/java/com/enac/vifa/vifa/Modele.java
+++ src/main/java/com/enac/vifa/vifa/Modele.java
@@ -1,10 +1,7 @@
 package com.enac.vifa.vifa;
 
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.function.Function;
-
-import javax.lang.model.element.Element;
+import java.util.Date;*
 
 import com.enac.vifa.vifa.formes.Moment3D;
 import com.enac.vifa.vifa.formes.Vecteur3D;
