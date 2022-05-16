643be05db54451c3a13458dd90fc2f30d0244ef4
HugoCourtadon <7723343-hugo.courtadon06@users.noreply.gitlab.com>
2022-05-16 14:42:52 +0200

    ThrottlePane.java : manette de gaz (supression de Gas.java)

--- src/main/java/com/enac/vifa/vifa/vues/ThrottlePane.java
+++ src/main/java/com/enac/vifa/vifa/vues/ThrottlePane.java
@@ -1,27 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.enac.vifa.vifa.vues;
 
 import com.enac.vifa.vifa.Modele;
-import com.enac.vifa.vifa.vues.ControllerPane;
-import com.enac.vifa.vifa.vues.Vue3D;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import static javafx.scene.layout.GridPane.setColumnIndex;
 import static javafx.scene.layout.GridPane.setRowIndex;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 /**
  *
  * @author hugocourtadon
  */
-public class Gas extends ControllerPane{
-    
+public class ThrottlePane extends ControllerPane {
     private final Label poussInfo;
     private final Slider pouss;
     
-    public Gas(Vue3D vue){
+    public ThrottlePane(Vue3D vue){
         super(vue);
         poussInfo = new Label("Poussée (de 0 à 1) : 0.0");
         setRowIndex(poussInfo,1);
