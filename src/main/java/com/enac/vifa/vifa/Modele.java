64013ee53f1a91ceb453b08a425fffdb0a5a0b1d
Victor755431 <11173617-vdecrevoisier@users.noreply.gitlab.com>
2022-05-16 15:55:36 +0200

    Une seule demande à la fois ?

--- src/main/java/com/enac/vifa/vifa/Modele.java
+++ src/main/java/com/enac/vifa/vifa/Modele.java
@@ -548,7 +548,7 @@ public class Modele {
         return (getForme3D(nom));
     }
 
-    public void getDescription() throws IvyException{
+    public synchronized void getDescription() throws IvyException{
         long temps = (new Date()).getTime();
         long tps;
         synchronized (verrouTemporelDescr){
@@ -611,7 +611,7 @@ public class Modele {
         }
     }
 
-    public void getForcesAndMoment (){
+    public synchronized void getForcesAndMoment (){
         
         
         long temps = (new Date()).getTime();
